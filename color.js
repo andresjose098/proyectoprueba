62a44aad3b8ec94cf1df837347cd6622ec0737fd
andres <andres-jose1998@hotmail.com>
2022-01-20 10:36:58 -0500

    realizado

---
+++ color.js
@@ -0,0 +1,19 @@
+
+
+function eventbutton1(){
+    document.getElementById("menu").style.backgroundColor="green";
+}
+
+
+function eventbutton2(){
+    document.getElementById("menu").style.backgroundColor="blue";
+}
+
+
+function eventbutton3(){
+    document.getElementById("menu").style.backgroundColor="red";
+}
+
+function eventbutton4(){
+    document.getElementById("menu").style.backgroundColor="gray";
+}
